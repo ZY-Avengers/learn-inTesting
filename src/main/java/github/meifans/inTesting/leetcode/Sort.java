2436b95bbafbcd7b7c4f614bd901e9b2bff26b19
pengfei zhao <pengfei zhao>
2017-04-23 23:36:45 +0800

    accepted twosum

--- src/main/java/github/meifans/inTesting/leetcode/Sort.java
+++ src/main/java/github/meifans/inTesting/leetcode/Sort.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 /**
  * Created by Meifans Zhao on 2017/4/22.
  */
+@Log
 public class Sort {
 
 
@@ -24,24 +25,48 @@ public class Sort {
     }
 
     public int[] insertion(int[] a ){
-        for (int i = 1; i < a.length-1; i++) {
+        if (a == null && a.length <= 1) {
+            return a;
+        }
+        int star = 1;
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                break;
+            }
+            star = i + 1;
+        }
+
+        log.info(star+"o");
+        for (int i = star; i < a.length-1; i++) {
+            int key = a[i];
             for (int j = i-1; j >=0; j--) {
-                if (a[j+1]<a[j]){
-                    a[j+1] = a[j+1] ^ a[j];
-                    a[j] = a[j+1] ^ a[j];
-                    a[j+1] = a[j+1] ^ a[j];
+                if (j == 0 && a[j] > key) {
+                    a[j] = a[j] ^ a[j + 1];
+                    a[j + 1] = a[j] ^ a[j + 1];
+                    a[j] = a[j] ^ a[j + 1];
+                    break;
+                }
+                if (a[j] > key) {
+                    a[j + 1] = a[j];
+                } else {
+                    a[j + 1] = key;
+                    break;
                 }
             }
         }
         return a;
     }
 
+
+
     @Log
     public static class SortTest {
 
         @Test
         public void testSort(){
-            int[] a = {3,2,7,4,6,4,8};
+//            int[] a = {2,3,2,7,4,6,4,8};
+            int[] a = {2,3,4,4,8};
+
             int[] actual = new Sort().insertion(a);
             for (int i : actual) {
                 log.info(""+i);
--- src/main/java/github/meifans/inTesting/leetcode/TwoSum.java
+++ src/main/java/github/meifans/inTesting/leetcode/TwoSum.java
@@ -1,5 +1,9 @@
 package github.meifans.inTesting.leetcode;
 
+import lombok.extern.java.Log;
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.util.Arrays;
 
 /**
@@ -9,10 +13,23 @@ public class TwoSum {
     public int[] twoSum(int[] numbers, int target) {
         if(numbers == null || numbers.length <2) return null;
         Arrays.sort(numbers);
-        for(int i =0; i<numbers.length; i++) {
-            int in = Arrays.binarySearch(numbers, target - numbers[i]);
 
+        for(int i =0; i < numbers.length ; i++) {
+            int in = Arrays.binarySearch(numbers, target - numbers[i]);
+            if( in < 0 || in ==i ) continue;
+            return new int[]{Math.min(i+1, in+1), Math.max(i+1,in+1)};
         }
         return null;
+    }
+
+    @Log
+    public static class TwoSumTest{
+
+        @Test
+        public void testTwoSum(){
+            int[] numbers = {5, 25, 75};
+            int[] r = new TwoSum().twoSum(numbers, 100);
+            Assert.assertArrayEquals(new int[]{2,3},r);
+        }
     }
 }
