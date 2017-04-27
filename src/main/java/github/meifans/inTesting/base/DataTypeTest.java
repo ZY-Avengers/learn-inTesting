1d6c00e648992d3ba5e41be565aeb98c70c663b1
siyuyu <siyuyu@creditease.cn>
2017-04-27 18:58:59 +0800

    invoke test

--- src/main/java/github/meifans/inTesting/base/DataTypeTest.java
+++ src/main/java/github/meifans/inTesting/base/DataTypeTest.java
@@ -1,6 +1,8 @@
 package github.meifans.inTesting.base;
 
 import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Set;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -32,4 +34,29 @@ public class DataTypeTest {
     long l = 12;
     double d = l;
   }
+
+  public static void main(String[] args) {
+    Class<?> c1 = null;
+    try {
+      c1 = Class.forName(args[0]);
+    } catch (ClassNotFoundException e) {
+      System.out.println("class not found");
+      System.exit(1);
+    }
+
+    Set<String> s = null;
+    try {
+      s = (Set<String>)c1.newInstance();
+    } catch (InstantiationException e) {
+      System.out.println("class not instantiable");
+      System.exit(1);
+    } catch (IllegalAccessException e) {
+      System.out.println("class not access");
+      System.exit(1);
+    }
+
+    s.addAll(Arrays.asList(args).subList(1, args.length));
+    System.out.println(s);
+  }
+
 }
