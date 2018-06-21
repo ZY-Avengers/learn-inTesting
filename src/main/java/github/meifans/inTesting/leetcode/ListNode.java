6a267b433cb7ef90432809725cb1ecabf2d543dc
Pengfei Zhao <916109363@qq.com>
2018-06-21 19:14:33 +0800

    update

--- src/main/java/github/meifans/inTesting/leetcode/ListNode.java
+++ src/main/java/github/meifans/inTesting/leetcode/ListNode.java
@@ -3,12 +3,12 @@ package github.meifans.inTesting.leetcode;
 /**
  * Created by Meifans Zhao on 2017/5/1.
  */
-class ListNode {
+public class ListNode {
 
     int val;
     ListNode next;
 
-    ListNode(int x) {
+    public ListNode(int x) {
         val = x;
     }
 }
--- src/main/java/github/meifans/inTesting/leetcode/famousAlgorithm/RegularExpressionMatching.java
+++ src/main/java/github/meifans/inTesting/leetcode/famousAlgorithm/RegularExpressionMatching.java
@@ -1,7 +1,9 @@
 package github.meifans.inTesting.leetcode.famousAlgorithm;
+
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
@@ -9,37 +11,78 @@ import java.util.Stack;
  * @author pengfei.zhao
  */
 public class RegularExpressionMatching {
-    final static int split = 256;
-    final static int matchstate = 257;
+    final static int start = 256;
+    final static int end = 257;
+    final static int any = 258;
 
-    public static boolean isMatch(String s, String p) {
-        Stack<Frag> fStack = new Stack<>();
-        char[] chars = s.toCharArray();
-        Frag e;
+    public static boolean isMatch(String str, String p) {
+        Stack<State> sStack = new Stack<>();
+        char[] chars = p.toCharArray();
+        State e;
         State state;
 
         for (char c : chars) {
             switch (c) {
                 case '*':
-                    e = fStack.pop();
-                    state = new State(split, e.start, null);
-                    state.patch(e.out);
-
+                    e = sStack.peek();
+                    e.out1 = e;
+                    break;
+                case '.':
+                    e = new State(any, null, null);
+                    sStack.push(e);
+                    break;
+                default:
+                    e = new State(c, null, null);
+                    sStack.push(e);
             }
         }
 
-        e = fStack.pop();
+        List<State> states = new ArrayList<>(sStack); //reverse stack
+        State start = null, pre, post;
+        for (int i = 0, len = states.size(); i < len; i++) {
+            if (i == 0 && !states.isEmpty()) {
+                start = states.get(0);
+            }
 
+            if (i + 1 < len) {
+                pre = states.get(i);
+                post = states.get(i + 1);
+                pre.out = post;
+            }
+        }
+
+        return match(start, str.toCharArray(), 0);
+    }
+
+
+    public static boolean match(State s, char[] chars, int i) {
+        if (s == null && i == chars.length) {
+            return true;
+        }
+
+        if (s != null && i < chars.length) {
+            return s.isMatch(chars[i]) && (
+                    match(s.out, chars, i + 1) || match(s.out1, chars, i + 1));
+        }
 
         return false;
+    }
 
+    @Test
+    public void test() {
+        Assert.assertFalse(isMatch("aa", "a"));
+        Assert.assertTrue(isMatch("aa", "aa"));
+        Assert.assertFalse(isMatch("aaa", "aa"));
+        Assert.assertTrue(isMatch("aa", "a*"));
+        Assert.assertTrue(isMatch("aa", ".*"));
+        Assert.assertTrue(isMatch("ab", ".*"));
+        Assert.assertTrue(isMatch("aab", "c*a*b"));
     }
 
     static class State {
         int c;
         State out;
         State out1;
-        int lastList;
 
         public State(int c, State out, State out1) {
             this.c = c;
@@ -47,30 +90,12 @@ public class RegularExpressionMatching {
             this.out1 = out1;
         }
 
-        public void patch(List<State> s) {
-            if (s.size() > 0) {
-                out = s.get(0);
-                if (s.size() > 1) {
-                    out1 = s.get(1);
-                }
-            }
+        public boolean isMatch(int c) {
+            if (this.c == any)
+                return true;
+
+            return this.c == c;
         }
-
     }
 
-    @Test
-    public void test() {
-        Assert.assertTrue(isMatch("aa", "a"));
-        Assert.assertTrue(isMatch("aa", "aa"));
-        Assert.assertTrue(isMatch("aaa", "aa"));
-        Assert.assertTrue(isMatch("aa", "a*"));
-        Assert.assertTrue(isMatch("aa", ".*"));
-        Assert.assertTrue(isMatch("ab", ".*"));
-        Assert.assertTrue(isMatch("aab", "c*a*b"));
-    }
-
-    static class Frag {
-        State start;
-        List<State> out;
-    }
 }
